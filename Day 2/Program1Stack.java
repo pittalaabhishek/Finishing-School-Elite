3a61b2e79dde3706503aa0c4410c49a17e4b66f0
Pittala Abhishek <pittalaabhishek999@gmail.com>
2021-12-14 16:10:46 +0530

    day 2 programs changed

---
+++ Day 2/Program1Stack.java
@@ -0,0 +1,81 @@
+/*
+Arjun is playing a word game in his tab. When he start the game , It displays a
+word and he has to check for any two adjacent characters which are same.If found 
+he has to select those two characters by tapping, so that both characters will
+be deleted from the word and the word size shrinks by 2. This process to be 
+repeated until the word has no two adjacent characters are same.
+You task is to help Arjun to perform the above task and Return the final word
+obtained.
+
+Input Format:
+-------------
+Line-1: A string represents the word.
+
+Output Format:
+--------------
+Return a string or empty string.
+
+Constraints:
+------------   
+    1 <= word.length <= 10^5   
+    word consists only lower case letters.
+
+Sample Input-1:
+---------------
+pqqprs
+
+Sample Output-1:
+----------------
+rs
+
+Explanation:
+-------------
+Initially , Delete two 'q's then the word will be 'pprs'. Now Delete two 'p's 
+then the word will be rs.
+
+Sample Input-2:
+---------------
+pqqqprrs
+
+Sample Output-2:
+----------------
+pqps
+
+Explanation:
+-------------
+Initially , Delete two 'q's then the word will be 'pqprrs'. Now Delete two 'r's
+then the word will be pqps.
+
+
+*/
+import java.util.*;
+import java.io.*;
+
+public class first{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+        String s=br.readLine();
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(stack.empty()){
+                stack.push(s.charAt(i));
+            }
+            else if(stack.peek()==s.charAt(i)){
+                stack.pop();
+            }
+            else{
+                stack.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb=new StringBuilder("");
+        while(!stack.empty()){
+            sb.append(stack.peek());
+            stack.pop();
+        }
+        sb.reverse();
+        bw.write(sb+"\n");
+        bw.flush();
+        //System.out.println(sb);
+    }
+}
