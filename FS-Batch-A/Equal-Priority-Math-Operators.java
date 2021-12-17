a10f5c706694da174c4ce766ac42dfb22f4472c1
Pittala Abhishek <pittalaabhishek999@gmail.com>
2021-12-17 14:51:35 +0530

    first command line commit

--- FS-Batch-A/Equal-Priority-Math-Operators.java
+++ FS-Batch-A/Equal-Priority-Math-Operators.java
@@ -34,77 +34,83 @@ Explanation :-
 -1 + -1 is -2 
 and -2 * 2 is -4
 */
+//Only single digits Present and Divided By zero Case ot tested
 
 import java.util.*;
-public class Main
-{
+
+public class Main {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String s=sc.next();
-		char[] arr=new char[100];
-		int[] brr=new int[100];
-		int l1=0,l2=0,p=0;
-		for(int i=1;i<s.length();i++){
-		    char c=s.charAt(i);
-		    if(c=='+'||c=='*'||c=='/'){
-		        arr[l1++]=c;
-		    }
-		    else if(c=='-'){
-		        int o=l1-1;
-		        if(o>=0&&arr[o]=='-'){
-		            continue;
-		        }else if(s.charAt(i-1)>='0'&&s.charAt(i-1)<='9'){
-		            arr[l1++]=c;
-		        }
-		    }
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		char[] arr = new char[100];
+		int[] brr = new int[100];
+		int l1 = 0, l2 = 0, p = 0;
+		for (int i = 1; i < s.length(); i++) {
+			char c = s.charAt(i);
+			if (c == '+' || c == '*' || c == '/') {
+				arr[l1++] = c;
+			} else if (c == '-') {
+				int o = l1 - 1;
+				if (o >= 0 && arr[o] == '-') {
+					continue;
+				} else if (s.charAt(i - 1) >= '0' && s.charAt(i - 1) <= '9') {
+					arr[l1++] = c;
+				}
+			}
 		}
-		int i=0,flag=0;
-		if(s.charAt(i)=='-'){
-		    i++;
-		    flag=1;
+		int i = 0, flag = 0;
+		if (s.charAt(i) == '-') {
+			i++;
+			flag = 1;
 		}
-		while(i<s.length()){
-		    char c=s.charAt(i);
-		    StringBuilder sb=new StringBuilder("");
-		    if(c!='+'&&c!='-'&&c!='*'&&c!='/'){
-		        int j=i;
-		        while(j<s.length()&&s.charAt(j)!='+'&&s.charAt(j)!='-'&&s.charAt(j)!='*'&&s.charAt(j)!='/'){
-		            sb.append(s.charAt(j));
-		            j++;
-		        }
-		        if(sb.length()>0){
-		            int n=Integer.parseInt(sb.toString());
-		            if(flag==1)n*=(-1);
-		            brr[l2++]=n;
-		            flag=0;
-		        }
-		        i=j;
-		    }else{
-		        if(flag==0&&s.charAt(i)=='-'&&(s.charAt(i-1)=='+'||s.charAt(i-1)=='-'||s.charAt(i-1)=='*'||s.charAt(i-1)=='/'))
-		        flag=1;
-		        i++;
-		    }
+		while (i < s.length()) {
+			char c = s.charAt(i);
+			StringBuilder sb = new StringBuilder("");
+			if (c != '+' && c != '-' && c != '*' && c != '/') {
+				int j = i;
+				while (j < s.length() && s.charAt(j) != '+' && s.charAt(j) != '-' && s.charAt(j) != '*'
+						&& s.charAt(j) != '/') {
+					sb.append(s.charAt(j));
+					j++;
+				}
+				if (sb.length() > 0) {
+					int n = Integer.parseInt(sb.toString());
+					if (flag == 1)
+						n *= (-1);
+					brr[l2++] = n;
+					flag = 0;
+				}
+				i = j;
+			} else {
+				if (flag == 0 && s.charAt(i) == '-' && (s.charAt(i - 1) == '+' || s.charAt(i - 1) == '-'
+						|| s.charAt(i - 1) == '*' || s.charAt(i - 1) == '/'))
+					flag = 1;
+				i++;
+			}
 		}
-		int res=brr[0];
-		for(i=1;i<brr.length;i++){
-		    if(arr[i-1]=='+'){
-		        res += brr[i];
-		    }else if(arr[i-1]=='-'){
-		        res -= brr[i];
-		    }else if(arr[i-1]=='*'){
-		        res *= brr[i];
-		    }else{
-		        if(brr[i]==0)continue;
-		        res /= brr[i];
-		    }
+		int res = brr[0];
+		for (i = 1; i < brr.length; i++) {
+			if (arr[i - 1] == '+') {
+				res += brr[i];
+			} else if (arr[i - 1] == '-') {
+				res -= brr[i];
+			} else if (arr[i - 1] == '*') {
+				res *= brr[i];
+			} else {
+				if (brr[i] == 0)
+					continue;
+				res /= brr[i];
+			}
 		}
-		/*System.out.println(l1-l2);
-		for(int l=0;l<l1;l++)
-		System.out.print(arr[l]+" ");
-		System.out.println();
-		for(int l=0;l<l2;l++)
-		System.out.print(brr[l]+" ");
-		System.out.println();*/
+		/*
+		 * System.out.println(l1-l2);
+		 * for(int l=0;l<l1;l++)
+		 * System.out.print(arr[l]+" ");
+		 * System.out.println();
+		 * for(int l=0;l<l2;l++)
+		 * System.out.print(brr[l]+" ");
+		 * System.out.println();
+		 */
 		System.out.print(res);
 	}
 }
